0c6dff50df975c5ddfc9ba0c596d58ba389d4150
mohith2017 <mohith.m.venkat@gmail.com>
2024-02-15 20:50:00 -0500

    Changed API URL

--- api/cohelm.ts
+++ api/cohelm.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 
 
 export async function getReportData() {
-    const response = await fetch("http://localhost:3000/api/cohelm");
+    const response = await fetch("https://cohelm-challenge.vercel.app/api/cohelm");
     const jsonData = await response.json();
     return jsonData;
   };
